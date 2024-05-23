7acb09ed46135a1b43ef762074714f7d88f9c032
notdule <148584497+notdule@users.noreply.github.com>
2024-05-23 23:18:39 +0200

    Code Update

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
+
+  updateUI(inputValues);
 }
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
-
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
   })
+}
+
+const bubbleSort = (array) => {
+
 }
 
 sortButton.addEventListener("click", sortInputArray);
